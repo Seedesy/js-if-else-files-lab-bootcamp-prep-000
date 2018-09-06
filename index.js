8ae9dd823649dd689f44919d105c302f10ca82da
nseedes@gmail.com <nseedes@gmail.com>
2018-09-06 11:57:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ module.exports = { name, height
 if (name === "Joe") {
   console.log("The name is correct")
 } else {
-if (height === 74){
+if (height === 74) {
   console.log("The height is correct")
 }
